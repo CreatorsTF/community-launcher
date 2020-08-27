fdd7419fa5b0d6b4a4d20b89630d4f9e7c865b78
Robert <rob5300@hotmail.com>
2020-08-27 23:50:15 +0100

    Correctly fixed install button not working.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,3 @@
-const ipcRenderer = window.ipcRenderer;
-
 var content = document.getElementById("content");
 var contentDummy = document.getElementById("contentdummy");
 var titleImage = document.getElementById("title-image");
@@ -19,11 +17,11 @@ var twitter = document.getElementById("socialMediaTwitter");
 var discord = document.getElementById("socialMediaDiscord");
 var instagram = document.getElementById("socialMediaInstagram");
 
-website.onclick = (handle, e) => { ipcRenderer.send("Visit-Mod-Social", "website"); };
-github.onclick = (handle, e) => { ipcRenderer.send("Visit-Mod-Social", "github"); };
-twitter.onclick = (handle, e) => { ipcRenderer.send("Visit-Mod-Social", "twitter"); };
-instagram.onclick = (handle, e) => { ipcRenderer.send("Visit-Mod-Social", "instagram"); };
-discord.onclick = (handle, e) => { ipcRenderer.send("Visit-Mod-Social", "discord"); };
+website.onclick = (handle, e) => { window.ipcRenderer.send("Visit-Mod-Social", "website"); };
+github.onclick = (handle, e) => { window.ipcRenderer.send("Visit-Mod-Social", "github"); };
+twitter.onclick = (handle, e) => { window.ipcRenderer.send("Visit-Mod-Social", "twitter"); };
+instagram.onclick = (handle, e) => { window.ipcRenderer.send("Visit-Mod-Social", "instagram"); };
+discord.onclick = (handle, e) => { window.ipcRenderer.send("Visit-Mod-Social", "discord"); };
 
 var installButton = document.getElementById("install-play-update");
 document.onload = () => {
@@ -57,72 +55,73 @@ function OnClick_Mod(data) {
 
     //Get the current state of this mod to set the name of the button correctly.
     //To do that, we tell the main process to set the current mod and set that up.
-    ipcRenderer.send("SetCurrentMod", data.name);
+    window.ipcRenderer.send("SetCurrentMod", data.name);
 }
 
 updateButton_Download.onclick = (downloadUpdate) => {
-  ipcRenderer.send("download_update");
-  window.log.info("User chose to download the update. Downloading it." + downloadUpdate);
+    window.ipcRenderer.send("download_update");
+    window.log.info("User chose to download the update. Downloading it." + downloadUpdate);
 }
 
 updateButton_Update.onclick = (closeProgramAndUpdate) => {
-  ipcRenderer.send("restart_app");
-  window.log.info("The launcher was restarted to update" + closeProgramAndUpdate);
+    window.ipcRenderer.send("restart_app");
+    window.log.info("The launcher was restarted to update" + closeProgramAndUpdate);
 }
 
-ipcRenderer.on("update_not_available", () => {
-  ipcRenderer.removeAllListeners("update_not_available");
-  updateButton_Updated.classList.remove("hidden");
-  updateButton_Download.remove();
-  updateButton_Downloading.remove();
-  updateButton_Update.remove();
-  window.log.info("No update available... Sad!");
+window.ipcRenderer.on("update_not_available", () => {
+    window.ipcRenderer.removeAllListeners("update_not_available");
+    updateButton_Updated.classList.remove("hidden");
+    updateButton_Download.remove();
+    updateButton_Downloading.remove();
+    updateButton_Update.remove();
+    window.log.info("No update available... Sad!");
 });
 
-ipcRenderer.on("update_available", () => {
-  ipcRenderer.removeAllListeners("update_available");
-  updateButton_Updated.remove();
-  updateButton_Download.classList.remove("hidden");
-  window.log.info("An update is available. Waiting for user's input to actually download it.");
+window.ipcRenderer.on("update_available", () => {
+    window.ipcRenderer.removeAllListeners("update_available");
+    updateButton_Updated.remove();
+    updateButton_Download.classList.remove("hidden");
+    window.log.info("An update is available. Waiting for user's input to actually download it.");
 });
 
-ipcRenderer.on("update_downloading", () => {
-  ipcRenderer.removeAllListeners("update_downloading");
-  updateButton_Download.remove();
-  updateButton_Downloading.classList.remove("hidden");
-  window.log.info("Downloading update");
+window.ipcRenderer.on("update_downloading", () => {
+    window.ipcRenderer.removeAllListeners("update_downloading");
+    updateButton_Download.remove();
+    updateButton_Downloading.classList.remove("hidden");
+    window.log.info("Downloading update");
 });
 
-ipcRenderer.on("update_downloaded", () => {
-  ipcRenderer.removeAllListeners("update_downloaded");
+window.ipcRenderer.on("update_downloaded", () => {
+    window.ipcRenderer.removeAllListeners("update_downloaded");
   updateButton_Downloading.remove();
   updateButton_Update.classList.remove("hidden");
   window.log.info("The update was downloaded and will be installed on restart. Waiting for user's input.");
 });
 
 document.getElementById("settings-button").addEventListener("click", (a,b) => {
-    ipcRenderer.send("SettingsWindow", "");
+    window.ipcRenderer.send("SettingsWindow", "");
 });
 
 document.getElementById("patchnotes-button").addEventListener("click", (a,b) => {
-    ipcRenderer.send("PatchNotesWindow", "");
+    window.ipcRenderer.send("PatchNotesWindow", "");
 });
 
 document.getElementById("serverlist").addEventListener("click", (a, b) => {
-    ipcRenderer.send("ServerListWindow", "");
+    window.ipcRenderer.send("ServerListWindow", "");
 });
 
 installButton.addEventListener("click", (e) => {
+    //Do NOT use e
     installButton.innerText = "STARTING...";
     installButton.disabled = true;
-    ipcRenderer.send("install-play-click", e);
+    window.ipcRenderer.send("install-play-click", ""); 
 });
 
-ipcRenderer.on("GetCurrentModVersion-Reply", (event, arg) => {
+window.ipcRenderer.on("GetCurrentModVersion-Reply", (event, arg) => {
     version.innerText = arg;
 });
 
-ipcRenderer.on("InstallButtonName-Reply", (event, arg) => {
+window.ipcRenderer.on("InstallButtonName-Reply", (event, arg) => {
     arg = arg.toLowerCase();
     installButton.innerText = arg.toUpperCase();
     if(arg != "installed" && arg != "internal error"){
@@ -161,10 +160,10 @@ ipcRenderer.on("InstallButtonName-Reply", (event, arg) => {
     }
 });
 
-ipcRenderer.on("FakeClickMod", (event, moddata) => {
+window.ipcRenderer.on("FakeClickMod", (event, moddata) => {
     OnClick_Mod(moddata);
 });
 
 removeButton.addEventListener("click", function(e) {
-    ipcRenderer.send("Remove-Mod", "");
+    window.ipcRenderer.send("Remove-Mod", "");
 });
