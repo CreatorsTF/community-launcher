ab2d45f0983c3cbe5ae462851c14ea18becc504f
Spencer <spencervess@gmail.com>
2020-09-02 11:11:33 -0400

    add comments to Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,11 @@
+// Gulp is used to move static files
+// I.E. html, css, images, etc.
+// Files to the build folder
+
 const gulp = require("gulp");
 
 exports.default = function () {
+  // Copies over html, css, and fonts, images, internal folders to build folder
   gulp
     .src(
       [
@@ -10,12 +15,16 @@ exports.default = function () {
         "src/images/**/*",
         "src/internal/**/*",
       ],
+      // Needed to keep folder structure
       { base: "src" }
     )
     .pipe(gulp.dest("build/"));
+  // Copies over package.json due to GetCurrentVersion reading it
   gulp.src("package.json").pipe(gulp.dest("build/"));
+  // Copies over only the needed node_modules css files to build folder
   return gulp
     .src(["node_modules/flag-icon-css/**/*", "node_modules/@mdi/**/*"], {
+      // Again, Needed to keep folder structure
       base: "node_modules",
     })
     .pipe(gulp.dest("build/node_modules"));
